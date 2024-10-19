b48ffb45109374f805edc9cb1a2f7c9eb2920ef5
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-10-19 23:53:34 +0530

    refactoring code for bugs, logic building

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -79,112 +79,145 @@ let order_books:{[key: string] : Order[]} = {
 
 
 // zod validation
-// middleware authentication
 
 orderRoute.post("/", (req:Request, res:Response) => {
-    const body = req.body;
     // if no limit present in property
-    if (!body.limit){
-        //market order
-        var qty = body.qty;
-        var ticker = body.ticker;
-        if (ticker in order_books)
+    
+    if (req.body.ticker in order_books)
+    {
+        if (!req.body.limit) 
         {
             const order_book = order_books.ticker;
             // Case 1: Market Order & Buy
-            if(body.type == "buy"){
+            if(req.body.type == Side.Buy)
+            {
+                return handleMarketBuyOrders(req, res, order_book);
+            }
+            else if (req.body.type == Side.Sell){
+                return handleMarketSellOrders(req, order_book);
+            }
+        }
+        else{
+            const order_book = order_books.ticker;
+            if(req.body.type == Side.Buy)
+            {
+                return handleLimitBuyOrders(req, order_book);
+            }
+            else if (req.body.type == Side.Sell){
+                return handleLimitSellOrders(req, order_book);
+            }
+        }
+    }
+    else
+    {
+        return res.status(403).json({msg: "Couldn't find the requested stock ticker!!!"});
+    }
+});
+
+function handleMarketBuyOrders(req:Request, res: Response, order_book:Order[])
+   {
                 const buyer:User = req.userDetail;
-                const asks:Order[] = order_book.filter((order) => {order.type == Side.Sell});
-                if(asks.length > 0){
+                const sellOrders:Order[] = order_book.filter((order) => {order.type == Side.Sell});
+                const buyQty = req.body.qty;
+                const ticker = req.body.ticker;
+                if(sellOrders.length > 0){
                     //sort the bids based on price
-                    asks.sort((o1, o2) => o2.price - o1.price);
-                    asks.map((ask) => {
-                        if(ask.qty > qty){
-                            if(buyer.balance >= ask.price*qty)
+                    var remaining_qty = buyQty;
+                    
+                    while (remaining_qty > 0){
+                        sellOrders.sort((o1, o2) => o2.price - o1.price);
+                        sellOrders.map((sellOrder) => {
+                            if (sellOrder.qty > buyQty)
                             {
-                                // First come First serve for stakeholders
-                                ask.stakeholders.sort((s1, s2) => {return s1.createdAt.getTime() - s2.createdAt.getTime();});
-
-                                // checking for qty
-                                const stakeholder: Stakeholder | undefined  = ask.stakeholders.find((stakeholder) => {return stakeholder.qty >= qty});
-                                let seller_id:number;
-                                if (stakeholder) {
-                                    seller_id = stakeholder.customer_id;
-                                    if(stakeholder.qty == qty) {
-                                        //remove stakeholder
-                                        ask.stakeholders.splice(ask.stakeholders.indexOf(stakeholder));
-                                    }
-                                    else {
-                                        //partial order serving
-                                        ask.stakeholders[ask.stakeholders.indexOf(stakeholder)].qty -= qty;
-                                    }
-
-                                    //handle buyer and seller data
-                                
-                                    buyer.balance -= ask.price*qty;
-                                    if (ticker in buyer.assets)
-                                    {
-                                        const ownedAssets: undefined | OwnedAssets = 
-                                        buyer.assets.get(ticker);
-                                        if(ownedAssets){
-                                            ownedAssets.qty += qty;
-                                            buyer.assets.set(ticker, ownedAssets);
-                                        }
-                                    }
-                                    else
-                                    {
-                                        buyer.assets.set(ticker, {qty : qty, avg_price: ask.price});
-                                    }
-
-                                    const seller:User | undefined = users.get(seller_id.toString());
-
-                                    if(seller)
-                                    {
-                                        seller.balance += ask.price*qty;
-                                        if (ticker in seller.assets)
-                                        {
-                                            const ownedAssets: undefined | OwnedAssets = 
-                                            buyer.assets.get(ticker);
-                                            if(ownedAssets){
-                                                ownedAssets.qty -= qty;
-                                                if(ownedAssets.qty == 0){
-                                                    seller.assets.delete(ticker);
-                                                }
-                                                else{
-                                                    seller.assets.set(ticker, ownedAssets);
-                                                }
-                                                
-                                            }
-                                        }
-                                        users.set(buyer.customer_id.toString(), buyer);
-                                        users.set(seller?.customer_id.toString(), seller);
-                                    }
-                                    
-                                    // handle order book data
-                                    order_book[order_book.indexOf(ask)].qty -= qty;
-                                    order_books[ticker] = order_book;
-                                    return res.status(200).json({
-                                        msg: "Txn Successful"
-                                    });
-
+                                if (buyer.balance >= sellOrder.price*buyQty){
+                                    validStakeholders:Stakeholder[]  = getValidStakeholdersFromSellOrders();
+                                    updateSellOrdersInOrdersMap();
                                 }
                                 else{
-                                    return res.status(403).json({msg: "No eligible seller found!"});
-                                }                              
+                                    return res.status(403).json({msg: "Insufficient wallet balance"});
+                                }
+                                
                             }
+                        });
+                    }
+                    
+                    asks.map((ask) => {
+                        if(ask.qty > qty && buyer.balance >= ask.price*qty) {
+                            // First come First serve for stakeholders
+                            
+                            ask.stakeholders.sort((s1, s2) => {return s1.createdAt.getTime() - s2.createdAt.getTime();});
+                            // checking for qty
+                            const stakeholder: Stakeholder | undefined  = ask.stakeholders.find((stakeholder) => {return stakeholder.qty >= qty});
+                            while (ask)
+                            let seller_id:number;
+                            if (stakeholder) {
+                                seller_id = stakeholder.customer_id;
+                                if(stakeholder.qty == qty) {
+                                    //remove stakeholder
+                                    ask.stakeholders.splice(ask.stakeholders.indexOf(stakeholder));
+                                }
+                                else {
+                                    //partial order serving
+                                    ask.stakeholders[ask.stakeholders.indexOf(stakeholder)].qty -= qty;
+                                }
+
+                                //handle buyer and seller data
+                                updateUserBalanceAndAssets(buyer, ask.price, qty, ticker, Side.Buy);
+
+                                const seller:any = users.get(seller_id.toString());
+                                
+                                updateUserBalanceAndAssets(seller, ask.price, qty, ticker, Side.Sell);
+                                
+                                // handle order book data
+                                order_book[order_book.indexOf(ask)].qty -= qty;
+                                order_books[ticker] = order_book;
+                                return res.status(200).json({
+                                    msg: "Txn Successful"
+                                });
+
+                            }
+                            else{
+                                //need to sell stocks of multiple stakeholders
+                            }                              
                         }
                         ask
                     });
                 }
 
-            }
-            // Case 2: Market Order & Sell 
-            
+            } 
+
+
+function updateUserBalanceAndAssets(user:User, price:number, qty: number, ticker: string, side: Side)
+{
+    if(side == Side.Buy) {
+        user.balance -= price*qty;
+        if (ticker in user.assets)
+        {
+            const ownedAssets:any = user.assets.get(ticker);
+            ownedAssets.qty += qty;
+            user.assets.set(ticker, ownedAssets);
         }
-        
+        else
+        {
+            user.assets.set(ticker, {qty : qty, avg_price: price});
+        }
     }
+    else {
+        user.balance += price*qty;
+        const ownedAssets:any = user.assets.get(ticker);
+        ownedAssets.qty -= qty;
+        if(ownedAssets.qty > 0){
+            user.assets.set(ticker, ownedAssets);
+        }
+        else 
+        {
+            user.assets.delete(ticker);
+        } 
+    }
+    users.set(user.customer_id.toString(), user);   
+}
 
-    return res.status(200).send();
+function getValidStakeholdersFromSellOrders(){
+    
+}
 
-
-});
